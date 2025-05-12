80b562a0fafe8a02864a04f1952acb00a6f278d5
tyavkd62 <107627503+tyavkd62@users.noreply.github.com>
2025-05-12 21:49:57 +0900

    5/12 session

--- app.py
+++ app.py
@@ -106,6 +106,9 @@ def custom_static(filename):
     return send_from_directory('static/img', filename)
 '''
 
+
+'''
+Buleprint
 from flask import Blueprint, Flask
 from auth.views import auth_blueprint
 from main.views import main_blueprint
@@ -118,3 +121,30 @@ app.register_blueprint(main_blueprint, url_prefix='/')
 @app.route('/welcome')
 def welcome():
     return '환영합니다! 이것은 블루프린트를 사용하지 않는 직접적인 라우트입니다.'
+'''
+
+from flask import Flask, session, abort
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route('/set_session')
+def set_session():
+    session['username'] = 'John'
+    return '세션에 사용자 이름이 설정되었습니다!'
+
+@app.route('/get_session')
+def get_session():
+    username = session.get('username')
+    if username:
+        return f'사용자 이름: {username}'
+    else:
+        return '사용자 이름이 세션에 설정되지 않았습니다.'
+    
+@app.route('/protected')
+def protected():
+    if 'username' not in session:
+        abort(403)
+        return '이 페이지는 로그인한 사용자만 볼 수 있습니다!'
+    else:
+        return '로그인된 페이지입니다!'
